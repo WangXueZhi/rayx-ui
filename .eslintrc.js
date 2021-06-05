ec3f77a20527fe07633131d5a1f145c0aa6abf97
XQ0118 <173141178@qq.com>
2021-06-05 14:52:52 +0800

    fix(build,js): eslint disable no-require

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,24 +2,26 @@ module.exports = {
   root: true,
   env: {
     node: true,
-    es2020: true,
+    es2020: true
   },
   extends: [
+    'plugin:@typescript-eslint/recommended',
     'eslint:recommended',
     'plugin:vue/essential',
-    'plugin:@typescript-eslint/recommended',
+    '@vue/standard'
   ],
   parserOptions: {
+    // parser: 'babel-eslint',
     ecmaVersion: 11,
     parser: '@typescript-eslint/parser',
-    sourceType: 'module',
+    sourceType: 'module'
   },
   plugins: ['vue', '@typescript-eslint'],
   rules: {
     'no-console': 'off',
     'no-debugger': 'off',
     camelcase: 0,
-    'no-var': 0, //禁用var，用let和const代替
-    quotes: [1, 'single'], //引号类型 `` "" ''
-  },
+    'no-var': 0, // 禁用var，用let和const代替
+    quotes: [1, 'single'] // 引号类型 `` "" ''
+  }
 }
