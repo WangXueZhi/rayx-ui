c0c91cc62d4f605cb320ebec16e00ffa8db9fc34
wangxuezhi <xuezhi.wang@keepsoft.net>
2021-07-07 17:23:15 +0800

    feat(modal): 模态框实现

--- packages/modal/README.md
+++ packages/modal/README.md
@@ -2,40 +2,143 @@
 
 # Modal
 
-模态窗，在弹出层上展示一些内容
+模态框，在弹出层上展示一些内容
 
 ## 示例
 
-示例1
+基础用法
 
 ```vue demo
 <template>
-  <div class="demo-wrapper">
-    <r-modal />
+  <div class="modal-demo-wrapper">
+    <r-modal v-model:show="show" :bodyOverflow="false">
+      <div class="modal-content-demo0">
+        <div class="modal-content-demo0-head">模态框基础用法</div>
+        <div class="modal-content-demo0-foot">
+          <r-button colorType="error" @click="show = false">关闭</r-button>
+        </div>
+      </div>
+    </r-modal>
+    <r-button @click="show = true">显示</r-button>
   </div>
 </template>
 <script>
-  export default {};
+export default {
+  data() {
+    return {
+      show: false
+    }
+  }
+}
 </script>
+<style>
+.modal-content-demo0 {
+  min-width: 200px;
+  background: #fff;
+  border: 1px solid #ccc;
+  padding: 15px;
+  border-radius: 2px;
+}
+.modal-content-demo0-foot {
+  display: flex;
+  margin-top: 20px;
+  justify-content: flex-end;
+}
+</style>
 ```
 
-示例2
+模态框动画
 
-```vue demo hidecode
+```vue demo
 <template>
-  <div class="demo-wrapper">
-    <r-modal />
+  <div class="modal-demo-wrapper">
+    <r-modal
+      v-model:show="show"
+      :bodyOverflow="false"
+      animateIn="bounceIn"
+      animateOut="bounceOut"
+    >
+      <div class="modal-content-demo1">
+        <div class="modal-content-demo1-head">模态框基础用法</div>
+        <div class="modal-content-demo1-foot">
+          <r-button colorType="error" @click="show = false">关闭</r-button>
+        </div>
+      </div>
+    </r-modal>
+    <r-button @click="show = true">显示</r-button>
   </div>
 </template>
 <script>
-  export default {};
+export default {
+  data() {
+    return {
+      show: false
+    }
+  }
+}
 </script>
+<style>
+.modal-content-demo1 {
+  min-width: 200px;
+  background: #fff;
+  border: 1px solid #ccc;
+  padding: 15px;
+  border-radius: 2px;
+}
+.modal-content-demo1-foot {
+  display: flex;
+  margin-top: 20px;
+  justify-content: flex-end;
+}
+</style>
+```
+
+点击遮罩层关闭
+
+```vue demo
+<template>
+  <div class="modal-demo-wrapper">
+    <r-modal
+      v-model:show="show"
+      :bodyOverflow="false"
+      animateIn="bounceIn"
+      animateOut="bounceOut"
+      closeOnClickMask
+    >
+      <div class="modal-content-demo1">
+        <div class="modal-content-demo1-head">点击遮罩层关闭</div>
+        <div class="modal-content-demo1-content">没有关闭按钮可咋整？</div>
+      </div>
+    </r-modal>
+    <r-button @click="show = true">显示</r-button>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      show: false
+    }
+  }
+}
+</script>
+<style>
+.modal-content-demo1 {
+  min-width: 200px;
+  background: #fff;
+  border: 1px solid #ccc;
+  padding: 15px;
+  border-radius: 2px;
+}
+.modal-content-demo1-head{
+  font-size: 16px;
+}
+.modal-content-demo1-content{
+  margin-top: 20px;
+  padding-bottom: 20px;
+}
+</style>
 ```
 
 <!-- props -->
-
-## event
-
-| 名称  | 说明 | 返回值 |
-| ----- | ---- | ------ |
-| 事件1 | 事件1 | null   |
+<!-- events -->
--- packages/modal/modal.vue
+++ packages/modal/modal.vue
@@ -1,24 +1,29 @@
 <template>
-  <transition
-    enter-active-class="animate__fadeIn"
-    leave-active-class="animate__fadeOut"
-    @before-leave="onClose"
-    @after-leave="afterLeave"
-  >
-    <div v-if="show" :class="['r-modal', { 'r-modal-mask': showMask }]">
-      <div class="r-modal-wrapper" style="">
-        <PopupBase
-          v-bind:show="show"
-          @update:show="(value) => $emit('update:show', value)"
+  <PopupBase v-bind:show="showPopupBase" class="r-modal" @onClose="onClose">
+    <div v-if="showMask" class="r-modal-mask" @click="clickMask"></div>
+    <div class="r-modal-wrapper">
+      <transition
+        :enter-active-class="`animate__${animateIn}`"
+        :leave-active-class="`animate__${animateOut}`"
+      >
+        <div
+          :class="[
+            'r-modal-content',
+            'animate__animated',
+            ...contentCustomClass
+          ]"
+          v-if="showModalContent"
         >
-        </PopupBase>
-      </div>
+          <slot></slot>
+        </div>
+      </transition>
     </div>
-  </transition>
+  </PopupBase>
 </template>
-<script>
+<script lang="ts">
+import { defineComponent, ref, nextTick, watch } from 'vue'
 import PopupBase from '../popup-base'
-export default {
+export default defineComponent({
   name: 'rModal',
   components: { PopupBase },
   props: {
@@ -33,6 +38,13 @@ export default {
      * 显示遮罩层
      */
     showMask: {
+      type: Boolean,
+      default: true
+    },
+    /**
+     * 点击遮罩层关闭
+     */
+    closeOnClickMask: {
       type: Boolean,
       default: false
     },
@@ -49,8 +61,78 @@ export default {
     animateOut: {
       type: String,
       default: 'fadeOut'
+    },
+    /**
+     *  是否给body设置overflow: hidden
+     */
+    bodyOverflow: {
+      type: Boolean,
+      default: true
+    },
+    /**
+     *  模态窗容器自定义class
+     */
+    contentCustomClass: {
+      type: Array,
+      default: (): string[] => []
     }
   },
-  methods: {}
-}
+  emits: {
+    /**
+     * 关闭事件
+     */
+    onClose: null,
+    /**
+     * 更新显示状态
+     */
+    'update:show': (isShow: boolean) => isShow
+  },
+  setup (props, ctx) {
+    const showModalContent = ref(false)
+    const showPopupBase = ref(false)
+    const modalContentRef = ref(null)
+
+    watch(
+      () => props.show,
+      async (show, prevShow) => {
+        if (show !== prevShow) {
+          if (show === true) {
+            showPopupBase.value = true
+            await nextTick()
+            showModalContent.value = true
+          } else {
+            showModalContent.value = false
+            await nextTick()
+            showPopupBase.value = false
+          }
+
+          if (props.bodyOverflow) {
+            show
+              ? document.body.classList.add('r-modal-parent--hidden')
+              : document.body.classList.remove('r-modal-parent--hidden')
+          }
+        }
+      }
+    )
+
+    // 点击遮罩层
+    const clickMask = () => {
+      if (props.closeOnClickMask) {
+        ctx.emit('update:show', false)
+      }
+    }
+
+    const onClose = () => {
+      ctx.emit('onClose')
+    }
+
+    return {
+      showPopupBase,
+      showModalContent,
+      modalContentRef,
+      clickMask,
+      onClose
+    }
+  }
+})
 </script>
